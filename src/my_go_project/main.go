230cd5c05da625430620935d5669f3d16738a564
surenderthakran <surender.thakran89@gmail.com>
2016-12-25 01:38:04 +0530

    applicaion port updated and go files formatted with gofmt

--- src/my_go_project/main.go
+++ src/my_go_project/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "fmt"
-  "html"
-  "log"
-  "net/http"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("inside /get")
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 
-    err := http.ListenAndServe(":8080", nil)  // Note: Not "localhost:8080" but ":8080"
-    log.Fatal(err)
+	err := http.ListenAndServe(":18770", nil) // Note: Not "localhost:8080" but ":8080"
+	log.Fatal(err)
 }
--- src/my_go_project/main_test.go
+++ src/my_go_project/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMain(t *testing.T) {
 
