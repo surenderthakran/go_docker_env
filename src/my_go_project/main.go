69ed93400a33a2a2d50b10b202145d226931d330
surenderthakran <surender.thakran89@gmail.com>
2017-05-03 18:48:50 +0530

    application response updated

--- src/my_go_project/main.go
+++ src/my_go_project/main.go
@@ -10,7 +10,8 @@ func main() {
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("inside /get")
 		fmt.Println(r)
-		fmt.Fprintf(w, "Hello, %q", r.URL.Query()["name"][0])
+		name := r.URL.Query()["name"][0]
+		fmt.Fprintf(w, "Hello, %q", name)
 	})
 
 	err := http.ListenAndServe(":18770", nil) // Note: Not "localhost:8080" but ":8080"
